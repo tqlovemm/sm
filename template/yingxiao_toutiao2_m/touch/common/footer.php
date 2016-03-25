9b040e4ac88b7c75159574c3c9d2ee081503d577
tqmm <397114202@qq.com>
2016-03-25 10:04:19 +0800

    射日弓fvsdf

--- template/yingxiao_toutiao2_m/touch/common/footer.php
+++ template/yingxiao_toutiao2_m/touch/common/footer.php
@@ -58,7 +58,7 @@ $('#fastsmilies').html('<table cellspacing="0" cellpadding="0" class="smilies"><
 }
 
 </script>
-
+<span style="display: none;"><script type="text/javascript">var cnzz_protocol = (("https:" == document.location.protocol) ? " https://" : " http://");document.write(unescape("%3Cspan id='cnzz_stat_icon_1258178229'%3E%3C/span%3E%3Cscript src='" + cnzz_protocol + "s95.cnzz.com/z_stat.php%3Fid%3D1258178229%26show%3Dpic' type='text/javascript'%3E%3C/script%3E"));</script></span>
 </body>
 </html>
 <!--{eval updatesession();}-->

e046020b62a594850f0f74c0a9575e0400981749
tqlmm <397114202@qq.com>
2016-10-25 13:24:57 +0800

    射日弓fvsdf

---
+++ source/module/contact/contact_index.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ *      [Discuz!] (C)2001-2099 Comsenz Inc.
+ *      This is NOT a freeware, use is subject to license terms
+ *
+ *      $Id: portal_index.php 31313 2012-08-10 03:51:03Z zhangguosheng $
+ */
+
+if(!defined('IN_DISCUZ')) {
+	exit('Access Denied');
+}
+
+include_once template('diy:contact/index');
+?>

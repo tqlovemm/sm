659d3979d26824d3b595a36fa190b80b901c64c9
tqlmm <397114202@qq.com>
2016-10-25 16:55:12 +0800

    射日弓fvsdf

--- template/yingxiao_toutiao2_m/touch/forum/forumdisplay_list.php
+++ template/yingxiao_toutiao2_m/touch/forum/forumdisplay_list.php
@@ -4,7 +4,24 @@
   <!--{loop $_G['forum_threadlist'] $key $thread}-->
     <li class="thlist"> <a href="forum.php?mod=viewthread&tid=$thread[tid]" $thread[highlight]>
     <h1> 
-
+      <!--{if $thread[folder] == 'lock'}-->
+      <span class="t_att">{echo m_lang('tlock')}</span>
+      <!--{elseif $thread['special'] == 1}-->
+      <span class="t_att">{echo m_lang('ts1')}</span>
+      <!--{elseif $thread['special'] == 2}-->
+      <span class="t_att">{echo m_lang('ts2')}</span>
+      <!--{elseif $thread['special'] == 3}-->
+      <span class="t_att">{echo m_lang('ts3')}</span>
+      <!--{elseif $thread['special'] == 4}-->
+      <span class="t_att">{echo m_lang('ts4')}</span>
+      <!--{elseif $thread['special'] == 5}-->
+      <span class="t_att">{echo m_lang('ts5')}</span>
+      <!--{elseif in_array($thread['displayorder'], array(1, 2, 3, 4))}-->
+      <span class="t_att">{echo m_lang('tdis')}</span>
+      <!--{elseif $thread['digest'] > 0}-->
+      <span class="t_att">{echo m_lang('tdig')}</span>
+      <!--{elseif $thread['attachment'] == 2 && $_G['setting']['mobile']['mobilesimpletype'] == 0}-->
+      <span class="t_att">{echo m_lang('tatt')}</span>
       <!--{else}--> 
       <!--{/if}--> 
       {$thread[subject]} </h1>
